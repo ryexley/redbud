b84b60b45852560229e7e2009dcdaee0f23c78be
Bob Yexley <bob@yexley.net>
2014-01-15 20:54:01 -0500

    Client script

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.use(app.router);
+app.use(express.static(__dirname + "/public"));
+app.use(express.errorHandler());
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+
 app.listen(3000);
 console.log("Redbud server listening on port 3000");
