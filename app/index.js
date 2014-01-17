0929d0dbe2b1257a678f2a27863ff2882dfbdc71
Bob Yexley <bob@yexley.net>
2014-01-17 10:56:25 -0500

    Response

--- app/index.js
+++ app/index.js
@@ -20,6 +20,7 @@ app.all("*", function (req, res, next) {
 
     res.set("Access-Control-Allow-Origin", "http://localhost:3000");
     res.set("Access-Control-Allow-Origin", "http://localhost:4000");
+    res.set("Access-Control-Allow-Origin", "http://redbud-sample.dev");
     res.set("Access-Control-Allow-Methods", "GET, POST");
     res.set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
 
@@ -36,6 +37,7 @@ app.use(app.router);
 
 app.post("/track", function (req, res, next) {
     db.saveTrackingData(req.body, function () {
+        res.send({ message: "success" });
         console.log("Tracking data saved");
     });
 });
