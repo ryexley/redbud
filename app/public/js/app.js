b8e97aa81f4c58f45a5cb44faa45869f51c372d7
Bob Yexley <bob@yexley.net>
2014-02-10 17:33:31 -0500

    Rollin'

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -5,11 +5,13 @@ define([
 ], function ($, _, ko) {
 
     var App = function () {
+        this.selectedApp = ko.observable();
         this.init();
     };
 
     _.extend(App.prototype, {
         init: function () {
+            this.selectedApp("Select an app");
             console.log("Redbud Analytics client app initialized...");
         }
     });
